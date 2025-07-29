92dd244e1416415c36ce7528ce2bcf490c393dd2
exedevcentral <exemetal@hotmail.com>
2025-07-29 16:35:44 -0300

    primer commit portal de noticias

---
+++ 1-modulos/funciones.js
@@ -0,0 +1,10 @@
+let nombre = "Exequiel Echevarria";
+
+export function cambiarNombre(nuevo) {
+    nombre = nuevo;
+}
+
+export function enviarMensaje() {
+    alert(nombre + " te ha enviado un mensaje");
+}
+
